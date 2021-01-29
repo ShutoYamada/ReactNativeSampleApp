e119e1386dba099efaaa951acf76930f5c3ac075
ShutoYamada <stym1122@gmail.com>
2021-01-29 11:09:02 +0900

    styled-componentsを追加

--- src/modules/Tasks/components/TaskCard.tsx
+++ src/modules/Tasks/components/TaskCard.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import {View, TouchableOpacity, Text} from 'react-native';
+import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {BlurView} from '@react-native-community/blur';
 import Task from '../objects/Task';
+
+const BlurCard = styled(BlurView)`
+  width: 100%;
+  border-radius: 20px;
+  margin-top: 1%;
+  margin-bottom: 1%;
+  padding: 10px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Check = styled.TouchableOpacity`
+  border-color: #00a960;
+  border-width: 2px;
+  border-radius: 10px;
+  padding: 8px;
+  margin-left: 1%;
+`;
+
+const TaskContent = styled.View`
+  margin-left: 2%;
+  width: 100%;
+`;
 
 type Props = {
   task: Task;
@@ -11,43 +35,14 @@ type Props = {
 const TaskCard: React.FC<Props> = (props: React.PropsWithChildren<Props>) => {
   const {task} = props;
   return (
-    <BlurView
-      blurAmount={1}
-      blurType="light"
-      style={{
-        width: '100%',
-        borderRadius: 20,
-        marginTop: '1%',
-        marginBottom: '1%',
-        padding: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-      }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}>
-        <TouchableOpacity
-          style={{
-            borderColor: '#00a960',
-            borderWidth: 2,
-            borderRadius: 10,
-            padding: 8,
-            marginLeft: '1%',
-          }}>
-          <Icon name="check" style={{color: '#00a960', fontSize: 16}} />
-        </TouchableOpacity>
-      </View>
-      <View
-        style={{
-          marginLeft: '2%',
-          width: '100%',
-        }}>
+    <BlurCard blurAmount={1} blurType="light">
+      <Check>
+        <Icon name="check" style={{color: '#00a960', fontSize: 16}} />
+      </Check>
+      <TaskContent>
         <Text style={{color: '#FFF', fontSize: 24}}>{task.name}</Text>
-      </View>
-    </BlurView>
+      </TaskContent>
+    </BlurCard>
   );
 };
 
