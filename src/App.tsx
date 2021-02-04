0b1b76926c1b7e076714b79923877a48b19ea4a0
ShutoYamada <stym1122@gmail.com>
2021-02-04 21:48:19 +0900

    TaskListの追加を反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   StatusBar,
-  Text,
-  View,
-  FlatList,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import {BlurView} from '@react-native-community/blur';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {Fab} from './modules/Commons';
-import {Task, TaskCard, TaskModal} from './modules/Tasks';
+import {Task, TaskModal, TaskList} from './modules/Tasks';
 
-declare const global: {HermesInternal: null | {}};
-
-const App = () => {
+const App: React.FC = () => {
   const data: Task[] = [
     {name: 'Some Task1', checked: true},
     {name: 'Some Task2', checked: false},
@@ -42,62 +33,12 @@ const App = () => {
       style={{height: '100%'}}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
-        <View style={{width: '90%', height: 'auto'}}>
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            style={{width: '100%', height: '100%', padding: '2%'}}
-            data={data}
-            keyExtractor={(d) => d.name}
-            renderItem={(item) => {
-              const task: Task = item.item;
-              return <TaskCard task={task} />;
-            }}
-          />
-        </View>
+        <TaskList taskList={data} />
         <Fab />
       </SafeAreaView>
       <TaskModal />
     </LinearGradient>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
