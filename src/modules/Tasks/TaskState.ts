7068f4caaa63b42b01b5f026ac165ca0e182a3a9
ShutoYamada <stym1122@gmail.com>
2021-02-06 22:49:12 +0900

    TaskStateを追加

--- src/modules/Tasks/TaskState.ts
+++ src/modules/Tasks/TaskState.ts
@@ -3,6 +3,10 @@ import Task from './objects/Task';
 class TaskState {
     /** タスク一覧 */
     list: Task[] = [];
+    /** タスク詳細 */
+    detail: Task | null = null;
+    /** 詳細モーダル表示 */
+    dispModal: boolean = false;
 }
 
 export default TaskState;
--- src/modules/Tasks/index.ts
+++ src/modules/Tasks/index.ts
@@ -3,9 +3,14 @@ import TaskCard from './components/TaskCard';
 import TaskCheckBox from './components/TaskCheckBox';
 import TaskModal from './components/TaskModal';
 import TaskList from './components/TaskList';
+import TaskState from './TaskState';
+import {TaskActions, TaskReducer} from './TaskSlice';
 
 export {
     Task,
+    TaskState,
+    TaskActions,
+    TaskReducer,
     TaskCard,
     TaskCheckBox,
     TaskModal,
