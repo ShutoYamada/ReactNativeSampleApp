2df5fce02400d9d8735c21ec60e2be8e09008a37
ShutoYamada <stym1122@gmail.com>
2021-02-06 22:49:00 +0900

    TaskActionを追加

--- src/modules/Tasks/TaskSlice.ts
+++ src/modules/Tasks/TaskSlice.ts
@@ -2,12 +2,26 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import TaskState from './TaskState';
 import Task from './objects/Task';
 
-const taskSlice = createSlice({
+const TaskSlice = createSlice({
     name: 'task',
     initialState: new TaskState(),
     reducers: {
+        /** タスク追加アクション */
         addList: (state: TaskState, action: PayloadAction<Task>) => {
             state.list = [...state.list, action.payload];
+        },
+        /** 詳細タスク設定アクション */
+        setDetail: (state: TaskState, action: PayloadAction<Task>) => {
+            state.detail = action.payload;
+        },
+        /** モーダル表示フラグ制御アクション */
+        setDispModal: (state: TaskState, action: PayloadAction<boolean>) => {
+            state.dispModal = action.payload;
         }
     }
-})
+});
+
+export const TaskActions = TaskSlice.actions;
+export const TaskReducer = TaskSlice.reducer;
+
+export default TaskSlice;
