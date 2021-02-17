21ccd5c1ccfc1f1733dbd8c9af2bbf6335e92e21
ShutoYamada <stym1122@gmail.com>
2021-02-17 17:37:58 +0900

    TaskScreenで扱うTaskのモックデータを修正

--- src/modules/Tasks/components/TaskScreen.tsx
+++ src/modules/Tasks/components/TaskScreen.tsx
@@ -1,19 +1,21 @@
 import React, {useCallback} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import { View } from 'react-native';
 import {TaskActions, Task, TaskModal, TaskList} from '../index';
-import {Main, Fab} from '../../Commons'
+import {Main, Fab} from '../../Commons';
 import {RootState} from '../../../Store';
 
 const TaskScreen: React.FC = () => {
-  const list: Task[] = useSelector(
-    (state: RootState) => state?.task?.list
-  );
-  
+  const list: Task[] = useSelector((state: RootState) => state?.task?.list);
+
   const dispatch = useDispatch();
   const createTask = useCallback(() => {
-    //const newTask: Task = new Task();
-    dispatch(TaskActions.openModal({checked: false, name: "hoge"}));
+    dispatch(
+      TaskActions.openModal({
+        name: 'hoge',
+        comment: 'some task',
+        checked: false,
+      }),
+    );
   }, [dispatch]);
 
   return (
