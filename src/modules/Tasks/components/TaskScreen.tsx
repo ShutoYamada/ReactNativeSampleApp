acfa4b4f7251fdafa22c34d3b827b636b9795c66
ShutoYamada <stym1122@gmail.com>
2021-02-18 10:59:26 +0900

    タスク画面において表示するリストをストレージから取得するよう修正

--- src/modules/Tasks/components/TaskScreen.tsx
+++ src/modules/Tasks/components/TaskScreen.tsx
@@ -1,22 +1,23 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {TaskActions, Task, TaskModal, TaskList} from '../index';
 import {Main, Fab} from '../../Commons';
 import {RootState} from '../../../Store';
+import {generateNewTask, loadTaskList} from '../TaskUtil';
 
 const TaskScreen: React.FC = () => {
   const list: Task[] = useSelector((state: RootState) => state?.task?.list);
 
   const dispatch = useDispatch();
   const createTask = useCallback(() => {
-    dispatch(
-      TaskActions.openModal({
-        name: 'hoge',
-        comment: 'some task',
-        checked: false,
-      }),
-    );
+    dispatch(TaskActions.createTask(generateNewTask(list)));
   }, [dispatch]);
+
+  useEffect(() => {
+    loadTaskList().then((all: Task[]) => {
+      dispatch(TaskActions.setList(all));
+    });
+  }, []);
 
   return (
     <Main>
