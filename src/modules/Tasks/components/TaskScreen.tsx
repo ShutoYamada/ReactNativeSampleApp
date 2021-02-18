de0dc67c0bf646f9e75aec129db2071e133de2a7
ShutoYamada <stym1122@gmail.com>
2021-02-18 13:29:20 +0900

    navigationを使ってStack系画面間で遷移ができることを確認

--- src/modules/Tasks/components/TaskScreen.tsx
+++ src/modules/Tasks/components/TaskScreen.tsx
@@ -1,16 +1,20 @@
 import React, {useCallback, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+import {useNavigation} from '@react-navigation/native';
 import {TaskActions, Task, TaskModal, TaskList} from '../index';
 import {Main, Fab} from '../../Commons';
 import {RootState} from '../../../Store';
 import {generateNewTask, loadTaskList} from '../TaskUtil';
 
-const TaskScreen: React.FC = () => {
+const TaskScreen: React.FC = (props) => {
   const list: Task[] = useSelector((state: RootState) => state?.task?.list);
-
+  const navigation = useNavigation();
   const dispatch = useDispatch();
   const createTask = useCallback(() => {
     dispatch(TaskActions.createTask(generateNewTask(list)));
+
+    // #F0F TODO これで遷移できる
+    //navigation.navigate('Setting');
   }, [dispatch]);
 
   useEffect(() => {
