ac835404911c87ab4eedc02877c8776133ad076e
ShutoYamada <stym1122@gmail.com>
2021-02-19 09:38:49 +0900

    グラデーションカラーを追加

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -2,8 +2,11 @@ const ActiveColor: string = '#00a960';
 const InactiveColor: string = '#444';
 const ErrorColor: string = '#F00';
 
+const FunnyGradation: string[] = ['#9890e3', '#f093fb'];
+
 export {
     ActiveColor,
     InactiveColor,
-    ErrorColor
+    ErrorColor,
+    FunnyGradation
 }
