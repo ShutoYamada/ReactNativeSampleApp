0d461a821a956dd0e64ec2e5b57508344d017569
ShutoYamada <stym1122@gmail.com>
2021-02-19 11:32:49 +0900

    MainのグラデーションカラーをStoreから取得するよう修正

--- src/modules/Commons/components/Main.tsx
+++ src/modules/Commons/components/Main.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
+import {useSelector} from 'react-redux';
+import {SafeAreaView, StatusBar} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import {RootState} from '../../../Store';
 
 const Main: React.FC = (props) => {
-    const {children} = props;
+  const {children} = props;
+  const selectedBgColor: string[] = useSelector(
+    (state: RootState) => state?.setting?.bgColor,
+  );
 
   return (
     <LinearGradient
       start={{x: 0, y: 0.75}}
       end={{x: 1, y: 0.25}}
-      colors={['#9890e3', '#f093fb']}
+      colors={selectedBgColor}
       style={{height: '100%'}}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{flex: 1, alignItems: 'center'}}>
