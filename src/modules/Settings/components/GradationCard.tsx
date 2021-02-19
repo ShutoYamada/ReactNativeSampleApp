f4113b70921bba557f49db0df04ae9bec156059d
ShutoYamada <stym1122@gmail.com>
2021-02-19 11:33:53 +0900

    Setting画面の背景色機能を一部実装

--- src/modules/Settings/components/GradationCard.tsx
+++ src/modules/Settings/components/GradationCard.tsx
@@ -1,8 +1,11 @@
 import React, {useCallback, useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import LinearGradient from 'react-native-linear-gradient';
 import {RootState} from '../../../Store';
+import {ActiveColor} from '../../../constants';
+import {SettingActions, SettingState} from '..';
 
 const Gradation = styled(LinearGradient)`
   width: 100%;
@@ -13,24 +16,41 @@ const GradationWrap = styled.TouchableOpacity`
   width: 30%;
 `;
 
+const SelectedIcon = styled(Icon)`
+  position: absolute;
+  top: 2%;
+  right: 2%;
+  font-size: 20px
+  color: ${ActiveColor};
+`;
+
 type Props = {
   colors: string[];
 };
 
 const GradationCard: React.FC<Props> = (props) => {
   const {colors} = props;
+  const settingState: SettingState = useSelector(
+    (state: RootState) => state?.setting,
+  );
+  const selectedBgColor: string[] = useSelector(
+    (state: RootState) => state?.setting?.bgColor,
+  );
   const dispatch = useDispatch();
   const onPress = useCallback(() => {
-    console.log('select color is ', colors);
-  }, [colors, dispatch]);
+    const newSettingState: SettingState = {
+      ...settingState,
+      bgColor: colors,
+    };
+    dispatch(SettingActions.setSetting(newSettingState));
+  }, [settingState, colors, dispatch]);
+  const isSelected: boolean = colors === selectedBgColor;
 
   return (
     <GradationWrap onPress={onPress}>
-      <Gradation
-        start={{x: 0, y: 0.75}}
-        end={{x: 1, y: 0.25}}
-        colors={colors}
-      />
+      <Gradation start={{x: 0, y: 0.75}} end={{x: 1, y: 0.25}} colors={colors}>
+        {isSelected ? <SelectedIcon name="check" /> : null}
+      </Gradation>
     </GradationWrap>
   );
 };
--- src/modules/Settings/components/SettingScreen.tsx
+++ src/modules/Settings/components/SettingScreen.tsx
@@ -4,7 +4,24 @@ import {useNavigation} from '@react-navigation/native';
 import styled from 'styled-components/native';
 import {Main, Fab, FabWrap, BlurContent} from '../../Commons';
 import {RootState} from '../../../Store';
-import {Text} from 'react-native';
+import {GradationCard} from '../';
+import {Text, View} from 'react-native';
+import {
+  PurpleGradation,
+  PinkGradation,
+  SunsetGradation,
+  RedGradation,
+  GreenGradation,
+  BlueGradation,
+} from '../../../constants';
+
+const GradationRow = styled.View`
+  margin-top: 2%;
+  margin-bottom: 2%;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
 const SettingScreen: React.FC = () => {
   const navigation = useNavigation();
@@ -16,6 +33,16 @@ const SettingScreen: React.FC = () => {
     <Main>
       <BlurContent blurAmount={1} blurType="light">
         <Text>hoge</Text>
+        <GradationRow>
+          <GradationCard colors={GreenGradation} />
+          <GradationCard colors={RedGradation} />
+          <GradationCard colors={BlueGradation} />
+        </GradationRow>
+        <GradationRow>
+          <GradationCard colors={PurpleGradation} />
+          <GradationCard colors={PinkGradation} />
+          <GradationCard colors={SunsetGradation} />
+        </GradationRow>
       </BlurContent>
       <FabWrap>
         <Fab onPressFab={back} iconName={'arrow-left'} />
