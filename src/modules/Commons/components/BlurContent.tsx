107afb65aa856c1b13cbfd80aa322fe2c35683cc
ShutoYamada <stym1122@gmail.com>
2021-02-19 10:34:28 +0900

    BlurContentを作成

---
+++ src/modules/Commons/components/BlurContent.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+import {BlurView} from '@react-native-community/blur';
+
+const BlurContent = styled(BlurView)`
+  width: 90%;
+  height: 100%;
+  border-radius: 20px;
+  margin-top: 1%;
+  margin-bottom: 1%;
+  padding: 10px;
+  align-items: center;
+`;
+
+export default BlurContent;
