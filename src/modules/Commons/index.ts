44a7e22517d3606d04e6635063e6db670d0e54de
ShutoYamada <stym1122@gmail.com>
2021-02-19 10:35:04 +0900

    BlurContentとFabWrapをindexに追加

--- src/modules/Commons/index.ts
+++ src/modules/Commons/index.ts
@@ -1,10 +1,13 @@
-import Fab from './components/Fab';
+import BlurContent from './components/BlurContent';
+import Fab, { FabWrap } from './components/Fab';
 import Main from './components/Main';
 import CommonButton from './components/CommonButton';
 import CommonTextInput from './components/CommonTextInput';
 
 export {
+    BlurContent,
     Fab,
+    FabWrap,
     Main,
     CommonButton,
     CommonTextInput
