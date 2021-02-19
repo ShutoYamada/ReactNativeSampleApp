7d82273a04c3bffde6f224e59737e86a9184785a
ShutoYamada <stym1122@gmail.com>
2021-02-19 13:04:51 +0900

    ピンクの色を調整

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -3,7 +3,7 @@ const InactiveColor: string = '#444';
 const ErrorColor: string = '#F00';
 
 const PurpleGradation: string[] = ['#9890e3', '#f093fb'];
-const PinkGradation: string[] = ['#fad0c4', '#ffd1ff'];
+const PinkGradation: string[] = ['#fad0c4', '#f5576c'];
 const SunsetGradation: string[] = ['#e14fad', '#f9d423'];
 const GreenGradation: string[] = ['#50cc7f', '#f5d100'];
 const RedGradation: string[] = ['#f9d423', '#f83600'];
